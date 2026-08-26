0e1cdde6c4fa41e1e004f901c6b2bb1e2e2eb63f
saket-0 <kumar.saketpal@gmail.com>
2026-08-26 14:13:34 +0530

    fix: add Access-Control-Allow-Headers to local server

--- fetch_images.py
+++ fetch_images.py
@@ -95,6 +95,8 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header('Access-Control-Allow-Private-Network', 'true')
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
         return super().end_headers()
         
